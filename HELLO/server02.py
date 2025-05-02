e3f54699d60896800aba0454c9c8ea557b3fedea
Victória Amaral <ttoiav.aamaral@gmail.com>
2025-05-01 23:13:28 -0300

    backend-w-python(hello): aprendendo sobre parametros.

--- HELLO/server02.py
+++ HELLO/server02.py
@@ -4,5 +4,12 @@ app = FastAPI()
 
 
 @app.get("/saudacao/{nome}") 
-async def root(nome): 
-    return nome
+async def root(nome: str):
+    texto = f'Olá {nome}, tudo em paz?' 
+    return {"mensagem": texto}
+
+@app.get("/quadrado/{numero}")
+def quadrado(numero:int):
+    resultado = numero * numero
+    texto = f'O quadrado de {numero} é {resultado}.'
+    return {"mensagem": texto}
